d37681336db620f3e5943360e6c6b644adf9248b
Tino <valennvillar@gmail.com>
2023-11-30 20:31:15 -0300

    newton method created

---
+++ nonlinear_methods.py
@@ -0,0 +1,23 @@
+def regula_falsi_method():
+
+
+def bisection_method():
+
+
+def secant_method():
+
+
+def newton_method(fnc, x0, iterations, tolerance):
+    x_prev = x0
+    current_iterations = 1
+    error = 1
+    while error > tolerance and current_iterations < iterations:
+        x_next = x_prev - (fnc(x_prev) / derive(fnc, x_prev))
+        error = (x_next - x_prev) / x_next
+        x_prev = x_next
+    return x_prev
+
+
+def derive(fnc, x):
+    h = 0.00001
+    return (fnc(x + h) - fnc(x)) / h
