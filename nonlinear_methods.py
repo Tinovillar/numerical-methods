9659f7d04256e79a0aeb7cfade943bd94cd42412
Tino <valennvillar@gmail.com>
2023-12-01 00:02:08 -0300

    secant method implemented

--- nonlinear_methods.py
+++ nonlinear_methods.py
@@ -18,18 +18,27 @@ def bisection_method(fnc, x_left, x_right, iterations, tolerance):
         return (x_left + x_right)/2
 
 
-def secant_method():
+def secant_method(fnc, x0, x1, iterations, tolerance):
+    current_iteration = 1
+    error = 1
+    while error > tolerance and current_iteration < iterations:
+        x2 = x1 - fnc(x1) * ( (x1-x0) / (fnc(x1) - fnc(x0)) )
+        x0 = x1
+        x1 = x2
+        error = abs((x1 - x0)/x1)
+        current_iteration += 1
+    return x1
 
 
 def newton_method(fnc, x0, iterations, tolerance):
     x_prev = x0
-    current_iterations = 1
+    current_iteration = 1
     error = 1
-    while (error > tolerance) and (current_iterations < iterations) and (derive(fnc, x_prev) != 0):
+    while (error > tolerance) and (current_iteration < iterations) and (derive(fnc, x_prev) != 0):
         x_next = x_prev - (fnc(x_prev) / derive(fnc, x_prev))
-        error = (x_next - x_prev) / x_next
+        error = abs((x_next - x_prev) / x_next)
         x_prev = x_next
-        current_iterations += 1
+        current_iteration += 1
     return x_prev
 
 
